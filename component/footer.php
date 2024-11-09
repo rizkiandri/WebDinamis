03ad650971662ed8455a8412cb31cd558150f648
rizkiandri <rizki9263@gmail.com>
2024-11-09 19:52:40 +0700

    rizki commit

---
+++ component/footer.php
@@ -0,0 +1,9 @@
+<footer class="fixed-bottom bg-dark text-light p-3 text-center">
+    <p>WEB DINAMIS-UTS @RAP</p>
+</footer>
+</body>
+<script src="DataTables/datatables.min.js"></script>
+<script type="text/javascript">
+    new DataTable("#MyTable");
+</script>
+</html>
---
+++ component/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UTS</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link href="DataTables/datatables.min.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">UTS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li> 
+                    <li class="nav-item">
+                    <a class="nav-link" href="msw.php">Mahasiswa</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+         </nav>
+    </header>
